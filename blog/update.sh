8f6c38936add315e9e83a47979e40d3d2620798b
Anton Gerdelan <antonofnote@gmail.com>
2017-06-22 20:42:32 +0100

    1498160551

--- blog/update.sh
+++ blog/update.sh
@@ -95,4 +95,4 @@ echo "</body></html>" >> $opf;
 # fix any relative paths to image folder
 # gsed is gnu-sed on mac after installing with brew
 gsed -i s@../images/@images/@g $opf
-#sed -i s@../images/@images/@g $opf
+sed -i s@../images/@images/@g $opf
